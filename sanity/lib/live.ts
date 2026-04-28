211d85ca59e99e6222530408a9d35bb1bf964905
raycodestar <raycodestar@gmail.com>
2026-04-28 04:39:22 +0300

    Fix: removed incompatible next-sanity/live module

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,9 +1,3 @@
-// Querying with "sanityFetch" will keep content automatically updated
-// Before using it, import and render "<SanityLive />" in your layout, see
-// https://github.com/sanity-io/next-sanity#live-content-api for more information.
-import { defineLive } from "next-sanity/live";
-import { client } from './client'
-
-export const { sanityFetch, SanityLive } = defineLive({
-  client,
-});
+// sanity/lib/live.ts
+export const sanityFetch = async () => { console.log('Live fetch not configured'); };
+export const SanityLive = () => null;
